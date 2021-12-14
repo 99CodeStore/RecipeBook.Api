5aaf82459edf0e4a097eb8fbcc63382d4ace0b2a
Bheemsen Singh <bheem_stn@hotmail.com>
2021-12-14 15:06:14 +0530

    Serilog Configuration for logging application log. and Adding Cors Policy for API

--- RecipeBook/Startup.cs
+++ RecipeBook/Startup.cs
@@ -1,16 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace RecipeBook
 {
@@ -28,9 +21,25 @@ namespace RecipeBook
         {
 
             services.AddControllers();
+            services.AddCors(setupAction =>
+                    {
+                        setupAction.AddPolicy("CorsPolicyAllowAll", policy =>
+                       {
+                           policy.AllowAnyOrigin()
+                         .AllowAnyMethod()
+                         .AllowAnyHeader();
+                       });
+                    });
+
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Recipe Book", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Recipe Book API",
+                    Version = "v1"
+                    ,
+                    Description = "Recipe Book-API provides REST-APIs for Receipe Book Application for various plateforms like Angular Apps, Android Apps etc. "
+                });
             });
         }
 
@@ -41,10 +50,12 @@ namespace RecipeBook
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RecipeBook v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Recipe Book - v1"));
             }
 
             app.UseHttpsRedirection();
+
+            app.UseCors("CorsPolicyAllowAll");
 
             app.UseRouting();
 
